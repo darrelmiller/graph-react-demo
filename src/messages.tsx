4807a7264899df41534e9521cdf92875f1e38ea9
Darrel Miller <darrmi@microsoft.com>
2020-02-13 10:08:00 -0500

    Updated messages

--- src/messages.tsx
+++ src/messages.tsx
@@ -2,7 +2,7 @@ import * as GraphModels from "@microsoft/microsoft-graph-types"
 import React, { useState, useEffect } from 'react';
 import { Table } from 'reactstrap';
 import moment from 'moment';
-import { getAllMessages } from './GraphService';
+import { getMessages } from './GraphService';
 import { Client } from "@microsoft/microsoft-graph-client";
 interface IMessagesProps {
   client: Client;
@@ -17,7 +17,7 @@ const Messages = (props: IMessagesProps) => {
   const [messages, setMessages] = useState<GraphModels.Message[]>([]);
   useEffect(() => {
     if(messages.length === 0) {
-      getAllMessages(props.client).then((mess) =>
+      getMessages(props.client).then((mess) =>
         setMessages(mess)
       );
     }

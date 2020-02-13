7733bc163e5dda53c0b4a1d7eb0b36ac06b37312
Darrel Miller <darrmi@microsoft.com>
2020-02-13 09:28:28 -0500

    before vincent messes up everything

---
+++ src/GraphService.ts
@@ -0,0 +1,66 @@
+import * as GraphModels from "@microsoft/microsoft-graph-types"
+import { PageIterator, PageIteratorCallback, PageCollection } from "@microsoft/microsoft-graph-client";
+
+
+export async function getUserDetails(client:any) {
+
+  const user = await client.api('/me').get();
+  
+  return user;
+}
+
+export async function getEvents(client:any) {
+  
+    const events:[GraphModels.Event] = await client
+      .api('/me/events')
+      .select('subject,organizer,start,end')
+      .orderby('createdDateTime DESC')
+      .get();
+  
+    return events;
+  }
+
+  export async function getMessages(client:any) {
+    const messages:[GraphModels.Message] = await client
+      .api('/me/messages')
+      .select('subject')
+      .orderby('createdDateTime DESC')
+      .get();
+      console.log(messages);
+      return messages;
+  }
+  export const getMessages2 = async (client:any): Promise<GraphModels.Message[]> => {
+    const messages: PageCollection = await client
+      .api('/me/messages')
+      .select('subject')
+      .orderby('createdDateTime DESC')
+      .get();
+      console.log(messages);
+      return messages.value as GraphModels.Message[];
+  }
+
+  export async function getAllMessages(client:any) {
+    const messageCollection:PageCollection = await client
+      .api('/me/messages')
+      .select('subject')
+      .orderby('createdDateTime DESC')
+      .get();
+
+      let allMessages = new Array<GraphModels.Message>()
+
+      let i = 0;
+      let callback: PageIteratorCallback = (data) => {
+           allMessages.push(data);
+          i++;
+          console.log(i);
+
+        return i < 5;
+      };
+      let pageIterator = new PageIterator(client, messageCollection, callback);
+      pageIterator.iterate();
+      return { value: allMessages} ;
+  }
+
+  export async function sendMail() {
+
+  }

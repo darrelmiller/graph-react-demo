96ab9c7c5105569a2140f839cd7615f77bbd9b92
Darrel Miller <darrmi@microsoft.com>
2020-02-13 09:41:52 -0500

    Vincent did magic

---
+++ src/messages.tsx
@@ -0,0 +1,48 @@
+import * as GraphModels from "@microsoft/microsoft-graph-types"
+import React, { useState, useEffect } from 'react';
+import { Table } from 'reactstrap';
+import moment from 'moment';
+import { getMessages2 } from './GraphService';
+import { Client } from "@microsoft/microsoft-graph-client";
+interface IMessagesProps {
+  client: Client;
+  showError: (message: string, debug: boolean) => void;
+}
+
+const Messages = (props: IMessagesProps) => {
+  // Helper function to format Graph date/time
+  const formatDateTime = (dateTime: any): string => {
+    return moment.utc(dateTime).local().format('M/D/YY h:mm A');
+  }
+  const [messages, setMessages] = useState<GraphModels.Message[]>([]);
+  useEffect(() => {
+    getMessages2(props.client).then((mess) =>
+      setMessages(mess)
+    );
+  }, [messages, props.client]);
+  return (
+      <div>
+        <h1>Messages</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th scope="col">Sent</th>
+              <th scope="col">Subject</th>
+            </tr>
+          </thead>
+          <tbody>
+            {messages.map(
+              (message) => {
+                return(
+                  <tr key={message.id}>
+                    <td>{formatDateTime(message.sentDateTime)}</td>
+                    <td>{message.subject}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+      </div>
+    );
+;}
+export default Messages;
